3c1a6f78c27cde1b84d00d288d4f59c6631e1dd0
Sector <makarovlanton@yandex.ru>
2018-01-29 11:32:04 +0300

    update

--- homework-3.php
+++ homework-3.php
@@ -1,5 +1,4 @@
 <?php
-
 $fauna = [
     "Africa" => [
         "Bufo bufo",
@@ -37,16 +36,15 @@ $fauna = [
         "Callicebus donacophilus"
     ]
 ];
-
 //basic task
 $new_animals = new_animals($fauna);
 
 //additional task
-print_animal($fauna);
-
+print_animal($new_animals);
 
 function new_animals($fauna){
     $animals = array();
+    $new_animals_pr = array();
     $new_animals = array();
     foreach ($fauna as $continent){
         foreach ($continent as $animal){
@@ -55,22 +53,35 @@ function new_animals($fauna){
             }
         }
     }
-    
+
     $first_word = array_column($animals, 0);
     $second_word = array_column($animals, 1);
     shuffle($second_word);
-
     echo "<h3>New animals: </h3>";
-
     for ($i = 0; $i < count($animals); $i++) {
         $a = $i + 1;
-        $new_animals[$i] = "<p style='padding: 0; margin: 0'>{$a}. {$first_word[$i]} {$second_word[$i]} </p>";
-        echo $new_animals[$i];
+        $new_animals_pr[$i] = "<p style='padding: 0; margin: 0'>{$a}. {$first_word[$i]} {$second_word[$i]} </p>";
+        echo $new_animals_pr[$i];
     };
 
+    foreach ($fauna as $continent => $animals) {
+        foreach ($animals as $animal) {
+            if (str_word_count($animal, 0) == 1){
+                $new_animals[$continent][] = $animal;
+            }
+
+            elseif (str_word_count($animal, 0) == 2){
+                for($i = 0; $i < count($first_word); $i++) {
+                    if (strpos($animal, $first_word[$i]) !== false) {
+                        $new_animals[$continent][] = $first_word[$i] . " " . $second_word[$i];
+                        break;
+                    }
+                }
+            }
+        }
+    }
     return $new_animals;
 }
-
 function print_animal($fauna){
     $continents = array();
     foreach ($fauna as $continent => $animal){
@@ -78,5 +89,4 @@ function print_animal($fauna){
         echo "<h2>$continent</h2>";
         echo (implode (", ", $animal)) . ".";
     }
-
 }
